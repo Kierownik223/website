b0cfb120366eec94fd262fa06ac36d23d7194f27
Kierownik223 <dev@marmak.net.pl>
2025-09-10 20:11:13 +0200

    Add dynamic equipment page

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const equipmentCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        category: z.string(),
+        description: z.string()
+    }),
+});
+
+export const collections = {
+    equipment: equipmentCollection
+};
