e1d208a66690824557d4fef0ff8fa2eb3df19ec2
Kierownik223 <marcel@marmak.net.pl>
2026-07-25 19:37:13 +0200

    Update collection to Astro v7

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,9 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const equipmentCollection = defineCollection({
+    loader: glob({ base: './src/content/equipment', pattern: '**/*.{md,mdx}' }),
     schema: z.object({
         priority: z.number().optional(),
         meaningful: z.boolean().optional(),
